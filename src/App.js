8367402c86baf8c3fbf5e54b220f780a867894ec
Tamara <toma.vashchuk@gmail.com>
2023-07-18 17:19:15 -0700

    Change dataflow and create onAddItems function

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const[items, setItems] = useState([])
+
+  function handleAddItems(item){
+    setItems(items=> [...items, item])
+  }
+
   return (
       <div className="app">
         <Logo />
-        <Form />
-        <PackingList />
+        <Form onAddItems={handleAddItems}/>
+        <PackingList items={items} />
         <Stats />
     </div>
   )
@@ -22,9 +28,10 @@ function Logo() {
   return <h1> ⛺️ PACKING LIST 🧳 </h1>
 }
 
-function Form() {
+function Form({onAddItems}) {
   const[description, setDescription] = useState("")
   const[quantity, setQuantity] = useState(1)
+
 
   function handleSubmit(e){
     if (!description) return;
@@ -32,6 +39,8 @@ function Form() {
 
     const newItem = {description, quantity, packed:false, id: Date.now()};
     console.log(newItem)
+
+    onAddItems(newItem)
 
     setDescription("")
     setQuantity(1)
@@ -59,10 +68,10 @@ function Form() {
   )
 }
 
-function PackingList() {
+function PackingList({items}) {
   return (
       <div className='list'>
-        <ul>{initialItems.map(item=> <Item item={item} key={item.id}/>)}
+        <ul>{items.map(item=> <Item item={item} key={item.id}/>)}
         </ul>
       </div>
   )
