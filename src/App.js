3823bfa76d8a5ecaf659af133502123d3c7d76e9
Tamara <toma.vashchuk@gmail.com>
2023-07-19 22:39:57 -0700

    Added function onToggleItem and also added checkbox for item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./index.css"
 import {useState} from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true }
-];
-
 export default function App() {
   const[items, setItems] = useState([])
 
@@ -14,11 +8,23 @@ export default function App() {
     setItems(items=> [...items, item])
   }
 
+  function handleDeleteItem(id){
+    setItems(items=>items.filter(items=>items.id !== id))
+  }
+
+  function handleToggleItem(id){
+    setItems(items =>
+        items.map(item =>
+            item.id === id ? {...item, packed: !item.packed} :
+                item)
+    );
+  }
+
   return (
       <div className="app">
         <Logo />
         <Form onAddItems={handleAddItems}/>
-        <PackingList items={items} />
+        <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
         <Stats />
     </div>
   )
@@ -68,21 +74,25 @@ function Form({onAddItems}) {
   )
 }
 
-function PackingList({items}) {
+function PackingList({items, onDeleteItem, onToggleItem}) {
   return (
       <div className='list'>
-        <ul>{items.map(item=> <Item item={item} key={item.id}/>)}
+        <ul>{items.map(item=> <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>)}
         </ul>
       </div>
   )
 }
 
-function Item({item}){
+function Item({item, onDeleteItem, onToggleItem}){
   return <li>
+    <input
+        type="checkbox"
+        value={item.packed}
+        onChange={()=>onToggleItem(item.id)}/>
     <span style={item.packed ? {textDecoration:"line-through"} : {}}>
       {item.quantity} {item.description}
     </span>
-    <button>❌</button>
+    <button onClick={()=>onDeleteItem(item.id)}>❌</button>
   </li>
 }
 
